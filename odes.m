e2c2a5512ce9a881f039c50e27443d527ec6f313
Raymond Gasper <raymond.j.gasper@gmail.com>
2018-02-13 14:13:34 -0500

    Update odes.m
    changed reaction rates to separated format

--- odes.m
+++ odes.m
@@ -8,10 +8,17 @@ function dydt = odes(t,y,flags,params)
  k3 = params(6);
  km3p = params(7);
  
+ r1 = k1p*y(1)*(1-y(3)-y(4));
+ rm1 = km1*y(3);
+ r2 = k2*y(3);
+ rm2 = km2*y(4);
+ r3 = k3*y(4);
+ rm3 = km3p*y(2)*(1-y(3)-y(4));
+ 
  % y(1) = psi-A, y(2) = psi-C, y(3) = theta-A, y(4) = theta-B
- dy1dt = alpha*( -k1p*y(1)*(1-y(3)-y(4)) + km1*y(3) );
- dy2dt = alpha*( k3*y(4) - km3p*y(2)*(1-y(3)-y(4)) );
- dy3dt = k1p*y(1)*(1-y(3)-y(4)) - km1*y(3) - k2*y(3) + km2*y(4);
- dy4dt = k2*y(3) - km2*y(4) - k3*y(4) + km3p*y(2)*(1-y(3)-y(4));
+ dy1dt = alpha*( -r1 + rm1 );
+ dy2dt = alpha*(  r3 - rm3 );
+ dy3dt = r1 - rm1 - r2 + rm2;
+ dy4dt = r2 - rm2 - r3 + rm3;
  
  dydt = [dy1dt ; dy2dt ; dy3dt; dy4dt]; % must return as column vector
